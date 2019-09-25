55d4d78eb909b5c750ee24501eff3822b3e65e08
Leandro <leandrojaviercepeda@gmail.com>
2019-09-25 16:48:40 -0300

    Guia ejercicios - TP5

--- tp5/app.js
+++ tp5/app.js
@@ -12,8 +12,12 @@ app.set("view engine", "hbs");
 //Handlebars properties
 Handlebars.registerPartials(__dirname + '/views/partials');
 
-//Renders
+//Renders Principal Views
 app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/home", (req, res) => {
   res.render("home");
 });
 
@@ -21,31 +25,55 @@ app.get("/blog", (req, res) => {
   res.render("blog");
 });
 
-app.get("/about", (req, res) => {
-  res.render("/partials/about");
-});
-
-app.get("/documentation", (req, res) => {
-  res.render("/partials/documentation");
-});
-
-app.get("/design_styles", (req, res) => {
-  res.render("/partials/design_styles");
-});
-
 app.get("/contact", (req, res) => {
   res.render("contact");
 });
 
-app.get("/docs", (req, res) => {
-  res.render("docs");
-});
-
 app.get("/portfolio", (req, res) => {
-  res.render("/partials/portfolio");
+  res.render("portfolio");
 });
 
 
+//Render Partials Views
+
+//Pages Partials
+app.get("/about", (req, res) => {
+  res.render("./partials/about");
+});
+
+app.get("/documentation", (req, res) => {
+  res.render("./partials/documentation");
+});
+
+app.get("/design_styles", (req, res) => {
+  res.render("./partials/design_styles");
+});
+
+//Portfolio Partials
+app.get("/portfolio-alt", (req, res) => {
+  res.render("./partials/portfolio-alt");
+});
+
+app.get("/portfolio-raw", (req, res) => {
+  res.render("./partials/portfolio-raw");
+});
+
+app.get("/portfolio-masonry", (req, res) => {
+  res.render("./partials/portfolio-masonry");
+});
+
+app.get("/portfolio-item", (req, res) => {
+  res.render("./partials/portfolio-item");
+});
+
+app.get("/portfolio-hover", (req, res) => {
+  res.render("./partials/portfolio-hover");
+});
+
+//Blog Partials
+app.get("/blog-post", (req, res) => {
+  res.render("./partials/blog-post");
+});
 
 //Listen app
 app.listen(port, () => {
